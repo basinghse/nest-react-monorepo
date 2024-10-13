610e61f63e83b7b007a5c7add6e736dd2fc32854
basinghse <bsinghv8@gmail.com>
2024-10-13 18:15:36 +1100

    Set API prefix

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // All backend requests wil be prefixed with /api
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
 bootstrap();
